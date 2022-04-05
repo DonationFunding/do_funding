bfe136a0318203e6d93e1b7f6ad2b3cbcda04c04
Yoon Kim <ep2525@naver.com>
2022-04-05 17:57:49 +0900

    haha

--- DoFunding/src/main/java/product/controller/ProductListController.java
+++ DoFunding/src/main/java/product/controller/ProductListController.java
@@ -1,21 +1,29 @@
 package product.controller;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import product.model.ProductBean;
 import product.model.ProductDao;
+import utility.Paging;
 
 @Controller
 public class ProductListController {
 
-	private final String command = "productList.prd";
+	private final String command = "list.prd";
 	private String getPage = "product_list";
 	// /WEB-INF/product/productInsertForm.jsp
-	private String gotoPage="redirect:/productList.prd";
+	private String gotoPage="redirect:/list.prd";
 
 	
 	@Autowired
@@ -26,9 +34,29 @@ public class ProductListController {
 
 	@RequestMapping(command)
 	public ModelAndView doAction(
+			@RequestParam(value="whatColumn", required=false) String whatColumn,
+			@RequestParam(value="keyword", required=false) String keyword,
+			@RequestParam(value="pageNumber", required=false) String pageNumber,
+			HttpServletRequest request
 			) {
+		Map<String, String> map=new HashMap<String, String>();
+		map.put("whatColumn", whatColumn);
+		map.put("keyword", "%"+keyword+"%");
+		
+		int totalCount=productDao.totalCount(map);
+		System.out.println("totalCount:"+totalCount);
+		
+		String url=request.getContextPath()+command;
+		Paging pageInfo=new Paging(pageNumber, "5", totalCount, url, whatColumn, keyword);
 	
+		  
+		List<ProductBean> list = productDao.productList(pageInfo, map);
+	 
 		ModelAndView mav=new ModelAndView();
+		mav.addObject("list",list);
+		mav.addObject("totalCount",totalCount);
+		mav.addObject("pageInfo",pageInfo);
+
 		mav.setViewName(getPage);
 		return mav;		
 	}
--- DoFunding/src/main/webapp/WEB-INF/common/common.jsp
+++ DoFunding/src/main/webapp/WEB-INF/common/common.jsp
@@ -6,9 +6,3 @@
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt" %>
 <%@ taglib prefix="fn" uri="http://java.sun.com/jsp/jstl/functions" %> 
-<<<<<<< HEAD
-=======
-
-접속자 아이디 : ${loginInfo.id }<br>
-       
->>>>>>> refs/remotes/origin/do-pms
