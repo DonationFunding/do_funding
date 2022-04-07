3679b0f83d61a204bf8598b58a9172fb2186b1fc
Minseo Park <pms8609@gmail.com>
2022-04-07 14:45:39 +0900

    a

--- DoFunding/src/main/java/admin/model/AdminBean.java
+++ DoFunding/src/main/java/admin/model/AdminBean.java
@@ -1,8 +1,5 @@
 package admin.model;
 
 public class AdminBean {
-<<<<<<< HEAD
-=======
 
->>>>>>> refs/remotes/origin/do-kyg
 }
--- DoFunding/src/main/java/admin/model/AdminDao.java
+++ DoFunding/src/main/java/admin/model/AdminDao.java
@@ -1,11 +1,8 @@
 package admin.model;
 
-<<<<<<< HEAD
 import org.springframework.stereotype.Component;
 
 @Component
-=======
->>>>>>> refs/remotes/origin/do-kyg
 public class AdminDao {
 
 }
