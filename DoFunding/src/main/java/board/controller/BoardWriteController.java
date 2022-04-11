a52c3b3d3cc1f21bc5915c07ec3b93da390cdd44
Yoon Kim <ep2525@naver.com>
2022-04-12 05:55:48 +0900

    d

--- DoFunding/src/main/java/board/controller/BoardWriteController.java
+++ DoFunding/src/main/java/board/controller/BoardWriteController.java
@@ -7,12 +7,9 @@ import java.sql.Timestamp;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
