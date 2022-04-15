a2303d2f8d74415b0d7ad7ad85565d23067f9a88
Yoon Kim <ep2525@naver.com>
2022-04-16 01:19:25 +0900

    deded

--- DoFunding/src/main/java/order/controller/DetailViewController.java
+++ DoFunding/src/main/java/order/controller/DetailViewController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import orderdetail.model.OrderDetailBean;
+import orderdetail.model.CompositeDao;
 
 
 @Controller
