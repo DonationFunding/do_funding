6c15d77010cc22af8866121a97a7a9a5355e4866
Yoon Kim <ep2525@naver.com>
2022-04-19 02:50:07 +0900

    1

--- DoFunding/src/main/java/dip/model/DipDao.java
+++ DoFunding/src/main/java/dip/model/DipDao.java
@@ -1,10 +1,18 @@
 package dip.model;
 
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component("myDipDao")
 public class DipDao {
 
+	@Autowired
+	SqlSessionTemplate sqlSessionTemplate;
+	
+	private String namespace="dip.model.Dip";	
+	
+	
 	public int checkDip(DipBean diBean) {
 		return 0;
 	}
