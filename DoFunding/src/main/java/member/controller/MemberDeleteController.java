262147138026afdfcdc03150b7f100ba749cadc2
pms86 <pms86@laptop>
2022-04-22 15:46:18 +0900

    회원정보에서 회원탈퇴 구현

--- DoFunding/src/main/java/member/controller/MemberDeleteController.java
+++ DoFunding/src/main/java/member/controller/MemberDeleteController.java
@@ -1,5 +1,8 @@
 package member.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,20 +14,20 @@ import member.model.MemberDao;
 @Controller
 public class MemberDeleteController {
 	private final String command = "/delete.mem";
-	private final String gotoPage = "redirect:/list.prd";
+	private final String gotoPage = "redirect:/logout.jsp";
 	
 	@Autowired
 	MemberDao mdao;
 	
 	@RequestMapping(value = command , method = RequestMethod.GET)
-	public String doAction(@RequestParam(value="no", required=false) String no) {
+	public String doAction(@RequestParam(value="no", required=false) String no,HttpSession session,HttpServletRequest request) {
+		System.out.println(request.getContextPath());
 		int cnt = mdao.deleteMember(no);
 		if(cnt < 0) {
 			System.out.println("삭제실패");
 			return gotoPage;
 		}
 		else {
-			System.out.println("삭제성공");
 			return gotoPage;
 		}
 		
