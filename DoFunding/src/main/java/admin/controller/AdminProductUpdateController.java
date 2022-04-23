4d3cab9c8a1947132ff43dab152c487430db9779
Yoon Kim <ep2525@naver.com>
2022-04-23 12:58:29 +0900

    관리자 상품 이미지 수정 관련 해결했습니다.

--- DoFunding/src/main/java/admin/controller/AdminProductUpdateController.java
+++ DoFunding/src/main/java/admin/controller/AdminProductUpdateController.java
@@ -7,6 +7,9 @@ import java.util.List;
 import java.util.Map;
 
 import javax.servlet.ServletContext;
+import javax.servlet.ServletRequest;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.jsp.jstl.core.Config;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -45,7 +48,7 @@ public class AdminProductUpdateController {
 	@RequestMapping(value=command, method = RequestMethod.GET)
 	public ModelAndView doAction(
 			@RequestParam(value="p_num", required = true) String num,
-			@RequestParam(value="pageNunber", required = false) String pageNumber
+			@RequestParam(value="pageNumber", required = false) String pageNumber
 			) {
 		//pageNumber
 		int p_num=Integer.parseInt(num);
@@ -65,14 +68,53 @@ public class AdminProductUpdateController {
 	@RequestMapping(value=command, method=RequestMethod.POST) 
 	 public ModelAndView doAction(
 			 @RequestParam(value="pageNunber", required = false) String pageNumber,
-			 @ModelAttribute("prdBean") ProductBean prdBean 
+			 @RequestParam(value="p_image1", required = true) String p_image1,
+			 @ModelAttribute("prdBean") ProductBean prdBean,
+			 HttpServletRequest request
 			 ) {	 
 		 ModelAndView mav = new ModelAndView();
 		 mav.addObject("pageNumber", pageNumber);
 		 List<CategoryBean> cateList = categoryDao.categoryAllByProduct();
 		 List<OptionBean> opList = productDao.optionAllByProduct(prdBean.getP_num());
-		 
 		 prdBean.setP_point(Math.round(prdBean.getP_origin_price()/1000)*10);
+		
+		 MultipartFile multi = prdBean.getUpload();
+		 String fileName = multi.getOriginalFilename();
+		 System.out.println("비었나:" + fileName.isEmpty());
+		 
+		 String uploadPath = servletContext.getRealPath("/resources/images");
+		 
+		 if(!fileName.isEmpty()) {	//업로드된게 있다면
+				uploadPath = servletContext.getRealPath("/resources/images");
+				File dir = new File(uploadPath+"\\" + prdBean.getP_image());
+				System.out.println("존재하는가" + dir.exists());
+				//동일한 이름의 이미지 삭제
+				if (dir.exists()) {
+					dir.delete();
+					System.out.println("삭제했는가 : " + !dir.exists());
+				}
+				//기존 이미지 삭제
+				File beforedir = new File(uploadPath+"\\" + p_image1);
+				if (beforedir.exists()) {
+					beforedir.delete();
+					System.out.println("삭제했는가 : " + !beforedir.exists());
+				}
+				
+				//새로이 받은 이미지 파일 추가하기
+				 File f = new File(uploadPath+"\\" + prdBean.getP_image());
+				 
+				 try {
+					multi.transferTo(f);
+				} catch (IllegalStateException e) {
+					e.printStackTrace();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}	
+		 }else {	//바꾼게 없다면
+			 prdBean.setP_image(p_image1);
+		 }
+		 
+		 	 
 		 int cnt = productDao.updateProduct(prdBean); 
 		 if(cnt > 0) {
 			 int item_no=productDao.getP_num();
@@ -88,21 +130,7 @@ public class AdminProductUpdateController {
 				 map.put("option_item_no", prdBean.getOption_item_no());
 				 productDao.itemOptionInsert(map);
 				} 
-			 
-		
-			 
-			 
-			 String uploadPath = servletContext.getRealPath("/resources/images");
-			 MultipartFile multi = prdBean.getUpload();
-			 File f = new File(uploadPath+"\\" + prdBean.getP_image());
-			 
-			 try {
-				multi.transferTo(f);
-			} catch (IllegalStateException e) {
-				e.printStackTrace();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}		 
+			 		 	 
 			 mav.setViewName(gotoPage);
 		 }
 		 else {
